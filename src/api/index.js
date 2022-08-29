9b6cdd4c2cab41ed2fc586817d75750a4764b3ba
Lassi Piironen <lsipii@kapsi.fi>
2022-08-29 21:09:43 +0300

    feat: update data access points

--- src/api/index.js
+++ src/api/index.js
@@ -4,9 +4,11 @@ const FIGURES_URL =
   'https://statfin.stat.fi/PXWeb/api/v1/fi/Kuntien_avainluvut/2021/kuntien_avainluvut_2021_aikasarja.px';
 const DATA_BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'http://localhost:8000' // ???
+    ? 'https://zbnjt92292.execute-api.eu-north-1.amazonaws.com/dev' // ???
     : 'http://localhost:8000';
-const DATA_URL = `${DATA_BASE_URL}/test/Figure/Population`;
+
+const DATA_ENDPOINT_PATH = 'test/lsipii/Figure/Population';
+const DATA_URL = `${DATA_BASE_URL}/${DATA_ENDPOINT_PATH}`;
 
 async function getKeyFigures() {
   return axios.get(`${FIGURES_URL}`);
