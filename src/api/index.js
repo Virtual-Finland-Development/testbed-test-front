0d7a8ab71009846775508c64d043b22ca0ee06a2
Lauri Saarela <lauri.saarela@gofore.com>
2022-08-31 10:14:42 +0300

    changed production endpoint / local endpoint for fetching data (testbed-test-api)

--- src/api/index.js
+++ src/api/index.js
@@ -4,24 +4,19 @@ const FIGURES_URL =
   'https://statfin.stat.fi/PXWeb/api/v1/fi/Kuntien_avainluvut/2021/kuntien_avainluvut_2021_aikasarja.px';
 
 /**
- * The endpoint url, not even the final form
+ * The endpoint url
  *
- * The production endpoint url would be: https://gateway.testbed.fi/test/lsipii/Figure/Population
- * -- but it does not yet work (for yet unresolved reasons) in browser requests
- *
- * @see: https://gateway.testbed.fi/docs#/Data%20Products/test_lsipii_Figure_Population_test_lsipii_Figure_Population_post
- * @see: https://definitions.testbed.fi/definitions/test/lsipii/Figure/Population
+ * testbed-test-api endpoint, serverless AWS function that routes api call to testbed environment
  */
 const PRODUCTION_ENDPOINT =
-  'https://zbnjt92292.execute-api.eu-north-1.amazonaws.com/dev'; // a backup endpoint url (the actual live data source)
-const DATA_ENDPOINT_PATH = 'test/lsipii/Figure/Population'; // The data path
-const DATA_QUERY_PARAMS = 'source=virtual_finland'; // The data source pin-pointer queryparam for the testbed
+  'https://9drrjton12.execute-api.eu-north-1.amazonaws.com'; // en
+const DATA_ENDPOINT_PATH = 'getPopulation'; // The data path
 
 const DATA_BASE_URL =
   process.env.NODE_ENV === 'production'
     ? PRODUCTION_ENDPOINT
-    : 'http://localhost:8000';
-const DATA_URL = `${DATA_BASE_URL}/${DATA_ENDPOINT_PATH}?${DATA_QUERY_PARAMS}`;
+    : 'http://localhost:3001';
+const DATA_URL = `${DATA_BASE_URL}/${DATA_ENDPOINT_PATH}`;
 
 async function getKeyFigures() {
   return axios.get(`${FIGURES_URL}`);
